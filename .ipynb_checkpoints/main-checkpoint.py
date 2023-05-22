70fa9b7dc48a2fab082b9d451429a88e4e3c6e46
jeahun10717 <jeahun10717@gmail.com>
2023-05-22 12:27:21 +0900

    merge crop code and main code

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -1,5 +1,7 @@
 import os
 
+
+from PIL import Image
 from extract_keywords import extract_cpp_keywords
 from myTurtle import Turtle
 
@@ -23,12 +25,46 @@ for file in codeList:
 
     T.done(file)
 
+def crop_image(image_path, file_name, keep_ratio):
+    # 이미지 로드
+    file_path = image_path + '/' + file_name
+    image = Image.open(file_path)
+
+    # 이미지의 너비와 높이 가져오기
+    width, height = image.size
+    
+    # 자르기를 원하는 비율 계산
+    crop_ratio = 1 - keep_ratio
+    
+    # 자를 영역 계산
+    left = int(width * crop_ratio / 2)
+    upper = int(height * crop_ratio / 2)
+    right = int(width * (1 - crop_ratio / 2))
+    lower = int(height * (1 - crop_ratio / 2))
+    
+    # 이미지 자르기
+    cropped_image = image.crop((left, upper, right, lower))
+
+    # 자른 이미지 저장
+    cropped_image.save("./crop_image/cp_" + file_name)
+    
+    
 imageList = os.listdir("image")
-i = 0
+# i = 0
 for file in imageList:
     if len(file.split('.')) == 3:
-        print(i, file)
-        i+=1
+        # print(i, file)
+        # i+=1
         filename = file.split('.')[2]
         if(filename == "svg"):
             os.remove("image/{}".format(file))
+
+codeList = os.listdir("image")
+imagePath = os.getcwd() + '/image'
+imageList = os.listdir(imagePath)
+
+for fileName in imageList:
+    if len(fileName.split('.')) == 3:
+        crop_image(imagePath, fileName, 0.6)
+    
+
