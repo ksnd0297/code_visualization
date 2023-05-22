85e5458a0116a1f86cf760359bb94c7c02d760f5
jeahun10717 <58159048+jeahun10717@users.noreply.github.com>
2023-05-22 12:56:11 +0900

    Feature/crop (#6)
    * modify myTurtle.py source code for svgtopng function
    
    * annotation deactivate
    
    * feature/pipPack
    
    * pip requirements upload
    
    * merge crop code and main code
    
    * comment upload

--- img_crop.py
+++ img_crop.py
@@ -1,22 +1,23 @@
 import os
 from PIL import Image
 
-def crop_image(image_path, file_name):
+def crop_image(image_path, file_name, keep_ratio):
     # 이미지 로드
     file_path = image_path + '/' + file_name
     image = Image.open(file_path)
 
-    # 이미지 크기 가져오기
+    # 이미지의 너비와 높이 가져오기
     width, height = image.size
-
+    
+    # 자르기를 원하는 비율 계산
+    crop_ratio = 1 - keep_ratio
+    
     # 자를 영역 계산
-    crop_width = width * 0.4
-    crop_height = crop_width
-
-    left = (width - crop_width) / 2
-    top = (height - crop_height) / 2
-    right = (width + crop_width) / 2
-    bottom = (height + crop_height) / 2
+    left = int(width * crop_ratio / 2)
+    upper = int(height * crop_ratio / 2)
+    right = int(width * (1 - crop_ratio / 2))
+    lower = int(height * (1 - crop_ratio / 2))
+    
 
     # 이미지 자르기
     cropped_image = image.crop((left, top, right, bottom))
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 
+
+from PIL import Image
 from extract_keywords import extract_cpp_keywords
 from myTurtle import Turtle
 
@@ -7,6 +9,8 @@ from myTurtle import Turtle
 
 codeList = os.listdir("code")
 
+# /code 폴더의 cpp 코드들 다 가져와서 turtle 로 그림 그리는 코드 
+# 여기서 svg랑 png 한 세트씩 만들어짐
 for file in codeList:
     if file.split('.')[-1] != 'cpp': continue
     code = open("{}".format("code/" + file), "r").read()
@@ -23,12 +27,51 @@ for file in codeList:
 
     T.done(file)
 
+# 이미지 자르는 함수
+def crop_image(image_path, file_name, keep_ratio):
+    # 이미지 로드
+    file_path = image_path + '/' + file_name
+    image = Image.open(file_path)
+
+    # 이미지의 너비와 높이 가져오기
+    width, height = image.size
+    
+    # 자르기를 원하는 비율 계산
+    crop_ratio = 1 - keep_ratio
+    
+    # 자를 영역 계산
+    left = int(width * crop_ratio / 2)
+    upper = int(height * crop_ratio / 2)
+    right = int(width * (1 - crop_ratio / 2))
+    lower = int(height * (1 - crop_ratio / 2))
+    
+    # 이미지 자르기
+    cropped_image = image.crop((left, upper, right, lower))
+
+    # 자른 이미지 저장
+    cropped_image.save("./crop_image/cp_" + file_name)
+    
+    
 imageList = os.listdir("image")
-i = 0
+
+# 아래 for 문은 /image 폴더에서 추후 사용하지 않을 svg 삭제하는 부분
 for file in imageList:
     if len(file.split('.')) == 3:
-        print(i, file)
-        i+=1
+        # print(i, file)
+        # i+=1
         filename = file.split('.')[2]
         if(filename == "svg"):
             os.remove("image/{}".format(file))
+
+
+imagePath = os.getcwd() + '/image'
+imageList = os.listdir(imagePath)
+
+# 아래 for 문은 crop_image 함수 호출하여 이미지 사이즈 리사이징 하는 부분
+for fileName in imageList:
+    if len(fileName.split('.')) == 3:
+        # image파일 경로, image파일 이름, 이미지 사이즈 퍼센트
+        # 이미지퍼센트는 60%를 남긴다는 뜻
+        crop_image(imagePath, fileName, 0.6) 
+    
+
